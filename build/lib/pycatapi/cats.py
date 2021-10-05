c355a2d084bb63d0620b56c2586683037239ec12
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-05 22:52:09 +0530

    Deleted dist, egg-ingo, build and __pycache__ folders

--- build/lib/pycatapi/cats.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-class Cat():
-    def get_cat():
-        """Get a random cat pic UwU"""
-        catraw = requests.get("https://api.thecatapi.com/v1/images/search").json()
-        cat = catraw[0]["url"]
-        return cat
-        
