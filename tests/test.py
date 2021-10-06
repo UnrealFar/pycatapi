3038729dcd25d52dbef6206ecf77318eb932e6bb
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-10-06 13:45:40 +0530

    Fix tests

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,8 @@
-from pycatapi import Cat
+from pycatapi import Client
 
-cat = Cat.get_cat()
+c = Client()
+
+cat = c.get_cat()
 print(cat)
+# Should print something similar to
+# Cat(breed=[], id="fghf", url="https://something", width=1000, height=250)
